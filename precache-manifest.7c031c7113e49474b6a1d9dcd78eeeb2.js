470f0fdf43133bba169a22359e3f5e43acf69577
Nahuel <nahuee.10.96@gmail.com>
2020-04-21 13:50:40 -0300

    Updates

---
+++ precache-manifest.7c031c7113e49474b6a1d9dcd78eeeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22449f0ff2273efc23d331685b77d514",
+    "url": "/Pixabay-Api-con-React/index.html"
+  },
+  {
+    "revision": "b97aca60e24a51eedf11",
+    "url": "/Pixabay-Api-con-React/static/css/main.11818b8b.chunk.css"
+  },
+  {
+    "revision": "cb2bcb661445c94761b6",
+    "url": "/Pixabay-Api-con-React/static/js/2.355a6d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pixabay-Api-con-React/static/js/2.355a6d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97aca60e24a51eedf11",
+    "url": "/Pixabay-Api-con-React/static/js/main.786c97c1.chunk.js"
+  },
+  {
+    "revision": "b7f8c27862138fa66f54",
+    "url": "/Pixabay-Api-con-React/static/js/runtime-main.9b7c7771.js"
+  }
+]);
